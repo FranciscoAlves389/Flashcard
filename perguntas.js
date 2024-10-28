622a2cee29904819f7fb8896e68bec028c219ffd
FranciscoAlves389 <franciscoalvess1314@gmail.com>
2024-10-27 22:06:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quais são as quatro operações matemáticas?',
+    'Adição, Subtração, Multiplicação e Divisão.'
+)
+
+criaCartao(
+    'Língua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (rai).'
+)
+
+criaCartao(
+    'Língua portuguesa',
+    'O que é verbo?',
+    'Verbo é uma classe de palavras que expressa ação, estado ou fenômeno.'
+)
+
+criaCartao(
+    'História',
+    'O que é comemorado no dia 7 de setembro no Brasil?',
+    'É comemorado o Dia da Independência.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'H₂O'
+)
+
+criaCartao(
+    'Física',
+    'Quantas cores tem o arco-íris?',
+    '7 cores.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'A fotossíntese é o processo em que plantas convertem luz solar em energia, produzindo glicose e oxigênio.'
+)
